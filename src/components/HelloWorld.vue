f97ef5cf613a80730394168dba1970c42ec0dd78
Jordan Hudgens <jordan@devcamp.com>
2018-05-26 10:22:10 -0500

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For guide and recipes on how to configure / customize this project,<br>
+      asdfasdfasd For guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
     </p>
@@ -30,11 +30,11 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
